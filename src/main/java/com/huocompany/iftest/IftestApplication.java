5e199675d21231b8d8dc4ca79cc0b323c6d3ff8b
ici <ici@DESKTOP-O8L8JOO>
2023-05-10 09:15:08 +0900

    test123

---
+++ src/main/java/com/huocompany/iftest/IftestApplication.java
@@ -0,0 +1,13 @@
+package com.huocompany.iftest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IftestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IftestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huocompany/iftest/IftestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huocompany.iftest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IftestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
